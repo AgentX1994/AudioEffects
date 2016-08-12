e0040cf0a01948c7c66aeec1e67d6672eb49c798
John Asper <agentx1994@gmail.com>
2016-08-11 21:01:12 -0400

    Initial Commit

---
+++ AudioEffects/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AudioEffects
+//
+//  Created by John Asper on 2016/8/11.
+//  Copyright © 2016 John Asper. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
